14a1446a5c34608d205a564b21e5abd45a4b6415
a17labs <*a17matte@gmail.com*>
2022-05-06 21:36:18 +0100

    changes

--- src/components/store/store.jsx
+++ src/components/store/store.jsx
@@ -3,7 +3,7 @@ import './store.css'
 
 const store = () => {
   return (
-    <div><video   loop={true} autoPlay="autoplay"  muted className='renders' src="/assets/b.mp4" type="video./mp4" /></div>
+    <div><video   loop={true} autoPlay="autoplay"  muted className='store' src="/assets/store.mp4" type="video./mp4" /></div>
   )
 }
 
