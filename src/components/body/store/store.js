a996079fd7da0bcef3eca69f9fed6aae0fb5d38e
a17labs <*a17matte@gmail.com*>
2022-11-02 02:07:40 +0000

    changes

--- src/components/body/store/store.js
+++ src/components/body/store/store.js
@@ -7,7 +7,8 @@ const store = () => {
  <div>  
     <div>
       <video   loop={true} data-autoplay data-keepplaying   muted className='store' src="/assets/store1.mp4" type="video./mp4" />
-      <div align="center"  classname="shopnow" ><h3 >RELEASE DATE AVAILABLE SOON</h3></div>
+      <div align="center"  classname="shopnow" ><h3 >RELEASE DATE</h3></div>
+      <div align="center"  classname="shopnow" ><h3 >07.07.23</h3></div>
     </div>
 </div> 
   )
